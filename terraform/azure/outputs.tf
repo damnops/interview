84c12716be268b5bca7257f102f120589d250dd9
sukai127 <sukai.snake@gmail.com>
2021-01-26 23:26:58 +0800

    [sukai] - change code to match provision-infra-via-terraform script

--- terraform/azure/outputs.tf
+++ terraform/azure/outputs.tf
@@ -3,9 +3,9 @@ output "public_ip" {
 }
 
 output "ssh_connection" {
-  value = "ssh azureuser@${azurerm_linux_virtual_machine.this.public_ip_address} -i ${path.cwd}/${path.module}/id_rsa_azure -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+  value = "ssh azureuser@${azurerm_linux_virtual_machine.this.public_ip_address} -i ${path.module}/tmp/id_rsa_azure -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
 }
 
 output "fetch_kubeconfig" {
-  value = "scp -i ${path.module}/id_rsa_azure -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null azureuser@${azurerm_linux_virtual_machine.this.public_ip_address}:/tmp/admin.kubeconfig ./kubeconfig"
+  value = "scp -i ${path.module}/tmp/id_rsa_azure -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null azureuser@${azurerm_linux_virtual_machine.this.public_ip_address}:/tmp/admin.kubeconfig ./kubeconfig"
 }
