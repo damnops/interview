2ba62e0c1e8ed296c10aab7bbae310a049913650
sh0werlee <showerlee@vip.qq.com>
2021-02-28 02:58:04 +0800

    Fix config for alicloud

--- terraform/alicloud/main.tf
+++ terraform/alicloud/main.tf
@@ -17,7 +17,7 @@ data "alicloud_zones" "default" {
 
 data "alicloud_images" "images" {
   most_recent = true
-  name_regex  = "^centos_7_*"
+  name_regex  = "^ubuntu_18_*"
 }
 
 
@@ -89,7 +89,7 @@ resource "alicloud_instance" "instance" {
     ]
   }
   provisioner "local-exec" {
-    command = "scp -i ./tmp/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${self.public_ip}:/tmp/admin.kubeconfig ./tmp/kube-config.txt"
+    command = "scp -i ./tmp/id_rsa -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null root@${self.public_ip}:/tmp/admin.kubeconfig ./tmp/kubeconfig.txt"
   }
 }
 
@@ -97,22 +97,45 @@ resource "alicloud_instance" "instance" {
 locals {
   user_data = <<EOF
 #!/bin/bash
+sudo -i
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
+
+apt-get update -y
+apt-get install git make vim python3 python3-pip sshpass curl rsync -y
+
 wget http://artifact.splunk.org.cn/yq/releases/download/v4.3.2/yq_linux_amd64 -O /usr/bin/yq
 chmod +x /usr/bin/yq
+
 ssh-keygen -t rsa -P "" -f ~/.ssh/id_rsa
 cat ~/.ssh/id_rsa.pub >> ~/.ssh/authorized_keys
-yum install git make vim -y
+
+pip3 install --upgrade pip
+pip3 install ansible==2.10.4
+
 git clone https://gitee.com/toc-lib/kube-ansible.git /usr/local/src/kube-ansible
 pushd /usr/local/src/kube-ansible
 cp group_vars/template.yml group_vars/all.yml
-IP=$(curl -s www.pubyun.com/dyndns/getip)
-yq eval --inplace "(.kubernetes.ssl.extension.[0]) = \"$IP\"" group_vars/all.yml
+PUBLIC_IP=$(curl -s www.pubyun.com/dyndns/getip)
+PRIVATE_IP=$(hostname -i)
+
+yq eval --inplace "(.kubernetes.ssl.extension.[0]) = \"$PUBLIC_IP\"" group_vars/all.yml
+yq eval --inplace '(.ansible_python_interpreter) = "/usr/bin/python3"' group_vars/all.yml
+yq eval --inplace '(.kubernetes.kubelet.pod_infra_container_image) = "registry.cn-hangzhou.aliyuncs.com/showerlee/pause:3.2"' group_vars/all.yml
+yq eval --inplace '(.kubernetes.settings.master_run_pod) = "true"' group_vars/all.yml
 yq eval --inplace '(.kubernetes.addon.image.metrics) = "registry.cn-beijing.aliyuncs.com/kubernetes_mirror_2021/metrics-server-amd64:v0.3.6"' group_vars/all.yml
 yq eval --inplace '(.kubernetes.addon.image.flannel) = "registry.cn-beijing.aliyuncs.com/kubernetes_mirror_2021/flannel:v0.12.0"' group_vars/all.yml
 yq eval --inplace '(.kubernetes.addon.image.canal.flannel) = "registry.cn-beijing.aliyuncs.com/kubernetes_mirror_2021/flannel:v0.12.0"' group_vars/all.yml
-yq eval --inplace '(.kubernetes.settings.master_run_pod) = "true"' group_vars/all.yml
-cp inventory/allinone.template inventory/hosts
-make runtime
+yq eval --inplace '(.kubernetes.addon.image.coredns) = "registry.cn-hangzhou.aliyuncs.com/showerlee/coredns:1.6.7"' group_vars/all.yml
+
+# init inventory file
+echo "[master]" > inventory/hosts
+echo "$PRIVATE_IP" >> inventory/hosts
+echo "[worker]" >> inventory/hosts
+echo "[kubernetes:children]" >> inventory/hosts
+echo "master" >> inventory/hosts
+echo "worker" >> inventory/hosts
+
 make install DOWNLOAD_WAY=qiniu | tee /tmp/kube-ansible.log
 popd
 EOF
