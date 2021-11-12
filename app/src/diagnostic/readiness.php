7aa565b59161c6350934766bf833fdb8f7520d7e
Zhu Ye <yezhu@thoughtworks.com>
2021-11-12 14:56:56 +0800

    Reset php files

--- app/src/diagnostic/readiness.php
+++ app/src/diagnostic/readiness.php
@@ -23,7 +23,3 @@ $conn = null;
 $rJSON = json_encode($checkResult);
 echo "$rJSON<br/>";
 ?>
-Host: 2604734e9f8d<br/>
-Host: e60658068fed<br/>
-Host: ba19a5ef14c7<br/>
-Host: 92b9f911b862<br/>
--- app/src/index.php
+++ app/src/index.php
@@ -52,7 +52,3 @@ catch(PDOException $e) {
 $conn = null;
 echo "</table>";
 ?>
-Host: 2604734e9f8d<br/>
-Host: e60658068fed<br/>
-Host: ba19a5ef14c7<br/>
-Host: 92b9f911b862<br/>
